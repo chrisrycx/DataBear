f02d4996dffa1e60b9eb247f7cc38efd9180b7f2
Chris Cox <chrisc@dyacon.com>
2020-07-22 17:08:35 -0600

    testing sockets in experiment

---
+++ experiments/sockets/socketdev.py
@@ -0,0 +1,23 @@
+'''
+Experimental script for developing sockets
+'''
+
+import socket
+
+HOST = 'localhost'  # Standard loopback interface address (localhost)
+PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen()
+    conn, addr = s.accept()
+    with conn:
+        print('Connected by', addr)
+        while True:
+            data = conn.recv(1024)
+            if not data:
+                break
+            print('I recieved {} bytes'.format(data))
+            conn.send(b'test')
+            conn.sendall(b'Good day!')
+            
