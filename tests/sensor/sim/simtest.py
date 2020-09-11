73440ccba1dbafdc1cbf6ec236546d31b4e9a9d1
chrisrycx <chrisrycx@gmail.com>
2020-09-11 14:50:28 -0600

    Updated sim sensor for newer interface

--- tests/sensor/sim/simtest.py
+++ tests/sensor/sim/simtest.py
@@ -5,7 +5,13 @@ A basic test of the sensor simulator
 
 #----- Import databear components ----
 from databear.sensors import simSensor
-from databear import logger,sensorfactory 
+from databear import logger,sensorfactory
+import os, importlib
+
+#----- Load a hardware driver -------
+drivername = os.environ['DBDRIVER']
+driver_module = importlib.import_module(drivername)
+driver = driver_module.dbdriver() 
 
 #-----  Register custom sensors with the sensor factory ----
 #import <module containing custom sensor class>
@@ -13,7 +19,7 @@ sensorfactory.factory.register_sensor('sensorSim',simSensor.sensorSim)
 
 #------ Create a logger ------
 config = 'simtest.yaml'
-datalogger = logger.DataLogger(config)
+datalogger = logger.DataLogger(config,driver)
 
 #------- Run databear ------
 #  ctrl-c to stop
