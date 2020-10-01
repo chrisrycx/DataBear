81104d0560bff4d03ed1b8fc1e10ec8b2cfe25ae
chrisrycx <chrisrycx@gmail.com>
2020-10-01 11:26:41 -0600

    Updated db schema with classes table.

--- databear/databearDB.sql
+++ databear/databearDB.sql
@@ -1,59 +1,73 @@
 BEGIN TRANSACTION;
-CREATE TABLE IF NOT EXISTS "sensors" (
-	"sensor_id"	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-	"name"	TEXT NOT NULL,
-	"serial_number"	TEXT NOT NULL,
-	"address"	INTEGER,
-	"virtualport"	INTEGER NOT NULL,
-	"sensor_type"	TEXT NOT NULL,
-	"description"	TEXT,
-	UNIQUE("serial_number","sensor_type")
-);
 CREATE TABLE IF NOT EXISTS "data" (
-	"dataid"	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+	"dataid"	INTEGER NOT NULL,
 	"dtstamp"	TEXT NOT NULL,
 	"value"	REAL NOT NULL,
 	"sensor_configid"	INTEGER NOT NULL,
 	"logging_configid"	INTEGER NOT NULL,
 	"qc_flag"	INTEGER,
 	FOREIGN KEY("logging_configid") REFERENCES "logging_configuration"("logging_configid") ON DELETE RESTRICT,
-	FOREIGN KEY("sensor_configid") REFERENCES "sensor_configuration"("sensor_configid") ON DELETE RESTRICT
+	FOREIGN KEY("sensor_configid") REFERENCES "sensor_configuration"("sensor_configid") ON DELETE RESTRICT,
+	PRIMARY KEY("dataid" AUTOINCREMENT)
 );
-CREATE TABLE IF NOT EXISTS "logging_configuration" (
-	"logging_configid"	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-	"measurementid"	INTEGER NOT NULL,
-	"storage_interval"	INTEGER NOT NULL,
-	"processid"	INTEGER NOT NULL,
-	"status"	INTEGER,
-	FOREIGN KEY("measurementid") REFERENCES "measurements"("measurement_id") ON DELETE CASCADE,
-	FOREIGN KEY("processid") REFERENCES "processes"("process_id") ON UPDATE CASCADE
+CREATE TABLE IF NOT EXISTS "processes" (
+	"process_id"	INTEGER NOT NULL,
+	"name"	TEXT NOT NULL,
+	"description"	TEXT NOT NULL,
+	PRIMARY KEY("process_id" AUTOINCREMENT)
+);
+CREATE TABLE IF NOT EXISTS "sensors_available" (
+	"sensorclass_id"	INTEGER,
+	"class_name"	TEXT NOT NULL,
+	"class_enabled"	INTEGER NOT NULL DEFAULT 0,
+	UNIQUE("class_name"),
+	PRIMARY KEY("sensorclass_id")
 );
 CREATE TABLE IF NOT EXISTS "sensor_configuration" (
-	"sensor_configid"	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-	"sensorid"	INTEGER NOT NULL,
+	"sensor_configid"	INTEGER NOT NULL,
+	"sensor_id"	INTEGER NOT NULL,
 	"measure_interval"	REAL NOT NULL,
 	"status"	INTEGER,
-	FOREIGN KEY("sensorid") REFERENCES "sensors"("sensor_id") ON DELETE CASCADE,
-	UNIQUE("sensorid","status")
+	FOREIGN KEY("sensor_id") REFERENCES "sensors"("sensor_id") ON DELETE CASCADE,
+	UNIQUE("sensor_id","status"),
+	PRIMARY KEY("sensor_configid" AUTOINCREMENT)
+);
+CREATE TABLE IF NOT EXISTS "logging_configuration" (
+	"logging_configid"	INTEGER NOT NULL,
+	"measurement_id"	INTEGER NOT NULL,
+	"sensor_id"	INTEGER NOT NULL,
+	"storage_interval"	INTEGER NOT NULL,
+	"process_id"	INTEGER NOT NULL,
+	"status"	INTEGER,
+	FOREIGN KEY("measurement_id") REFERENCES "measurements"("measurement_id") ON DELETE CASCADE,
+	FOREIGN KEY("process_id") REFERENCES "processes"("process_id") ON UPDATE CASCADE,
+	FOREIGN KEY("sensor_id") REFERENCES "sensors"("sensor_id") ON DELETE CASCADE,
+	PRIMARY KEY("logging_configid" AUTOINCREMENT)
+);
+CREATE TABLE IF NOT EXISTS "sensors" (
+	"sensor_id"	INTEGER NOT NULL,
+	"name"	TEXT NOT NULL,
+	"serial_number"	TEXT NOT NULL,
+	"address"	INTEGER NOT NULL DEFAULT 0,
+	"virtualport"	INTEGER NOT NULL,
+	"class_name"	TEXT NOT NULL,
+	"description"	TEXT,
+	FOREIGN KEY("class_name") REFERENCES "sensors_available"("class_name") ON DELETE CASCADE,
+	UNIQUE("serial_number","class_name"),
+	PRIMARY KEY("sensor_id" AUTOINCREMENT)
 );
 CREATE TABLE IF NOT EXISTS "measurements" (
-	"measurement_id"	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-	"sensor_id"	INTEGER NOT NULL,
+	"measurement_id"	INTEGER,
 	"name"	TEXT NOT NULL,
 	"units"	TEXT NOT NULL,
 	"description"	TEXT,
-	FOREIGN KEY("sensor_id") REFERENCES "sensors"("sensor_id") on update restrict
+	"class_name"	TEXT,
+	FOREIGN KEY("class_name") REFERENCES "sensors_available"("class_name") ON DELETE CASCADE,
+	PRIMARY KEY("measurement_id")
 );
-CREATE TABLE IF NOT EXISTS "processes" (
-	"process_id"	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-	"name"	TEXT NOT NULL,
-	"description"	TEXT NOT NULL
-);
-INSERT INTO "sensors" ("sensor_id","name","serial_number","address","virtualport","sensor_type","description") VALUES (1,'simulator','0',NULL,'port0','dbSim','Default simulator sensor');
-INSERT INTO "measurements" ("measurement_id","sensor_id","name","units","description") VALUES (1,1,'simsecond','second','The first simulator measurement.');
-INSERT INTO "processes" ("process_id","name","description") VALUES (1,'Sample','Select the first measurement from storage interval for storage'),
- (2,'Average','Calculate the average value of measurements from storage interval'),
- (3,'Max','Select the maximum value from measurement in the storage interval'),
- (4,'Min','Select the minimum value from measurement in the storage interval'),
- (5,'Dump','Select all measurements from the storage interval for storage');
+INSERT INTO "processes" VALUES (1,'Sample','Select the first measurement from storage interval for storage');
+INSERT INTO "processes" VALUES (2,'Average','Calculate the average value of measurements from storage interval');
+INSERT INTO "processes" VALUES (3,'Max','Select the maximum value from measurement in the storage interval');
+INSERT INTO "processes" VALUES (4,'Min','Select the minimum value from measurement in the storage interval');
+INSERT INTO "processes" VALUES (5,'Dump','Select all measurements from the storage interval for storage');
 COMMIT;
